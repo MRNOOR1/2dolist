fd275ba77f9a9c8328cf4a9df867d8e31367f679
Mohammad rasoul Noori <mohammadnoori812@mail.com>
2024-04-08 14:38:35 +1000

    Initial Commit

---
+++ 2dolist/_dolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  _dolistApp.swift
+//  2dolist
+//
+//  Created by Mohammad Rasoul Noori on 8/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct _dolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
